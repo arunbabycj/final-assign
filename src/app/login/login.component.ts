e7b6fb8d973d9747b371f80ca2a3d25e94e9ad3c
Divya Arunbaby <divyaarunbaby@Divyas-MacBook-Air.local>
2018-09-01 17:41:36 +1000

    working as normal chat

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent implements OnInit {
+    username : string = "";
+    password : string = "";
+  constructor(private router:Router, private form:FormsModule) { }
 
   ngOnInit() {
   }
 
+  loginUser(event){
+    event.preventDefault();
+    if(this.username != "" && this.password != ""){
+      if(typeof(Storage) != "undefined"){
+        console.log("storage ready");
+        //name = this.username;
+        //console.log(this.username);
+        sessionStorage.setItem("username", this.username);
+        //console.log(sessionStorage.getItem("username"));
+      } else {
+      //  alert("sessionStorage not available");
+      }
+      this.router.navigateByUrl('/chat');
+    } else {
+      alert("Username and password were incorrect");
+    }
+  }
+/*
+  loginUser(event){
+    event.preventDefault();
+    if(typeof(Storage) != "undefined"){
+      console.log("storage ready");
+      //name = this.username;
+      sessionStorage.setItem("username", this.username);
+      console.log(sessionStorage.getItem("username"));
+    } else {
+      alert("sessionStorage not available");
+    }
+  }*/
 }
--- src/app/services/socket/socket.service.ts
+++ src/app/services/socket/socket.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import * as io from 'socket.io-client';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocketService {
-
+  private url = "http://localhost:3000";
+  private socket;
   constructor() { }
+  sendMessage(message){
+    this.socket.emit('add-message', message);
+  }
+  getMessages(){
+    let obmessages = new Observable(
+      //'observer' is a javascript object that defines the handlers for the
+      //notifications that we will receive
+      observer => {
+      this.socket = io();
+      //listen for 'new-message'event from the Server
+      this.socket.on('message',(data)=> {observer.next(data);});
+
+      //when the observer ends (unsubscribe) then disconnet the Socket
+      return ()=>{this.socket.disconnect();}
+    })
+    return obmessages;
+  }
+
 }
